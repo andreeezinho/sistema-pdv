4b522426726d50f058f9a5973caee9926e8a34fd
andreeezinho <andreeesapucaia@gmail.com>
2025-05-31 14:42:54 -0300

    feat: add last-sales in dashboard view

--- src/Resources/Views/dashboard/index.php
+++ src/Resources/Views/dashboard/index.php
@@ -75,11 +75,36 @@
 
     <div class="flex gap-x-2">
         <div class="bg-neutral-50 p-4 rounded-lg mt-5 w-1/2">
-        
+            <div class="flex mb-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2 my-auto">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
+                <h3 class="text-2xl font-bold tracking-tight text-gray-900">Últimas vendas</h3>
+            </div>
+
+            <div class="flex px-2 border-b border-gray-200 my-4">
+                <p class="w-[25%] text-gray-500 border-r border-gray-200 pl-2">Código</p>
+                <p class="w-[25%] text-gray-500 border-r border-gray-200 pl-2">Produtos</p>
+                <p class="w-[25%] text-gray-500 border-r border-gray-200 pl-2">Total</p>
+                <p class="w-[25%] text-gray-500 pl-2">Pagamento</p>
+            </div>
+
+            <div class="flex bg-white p-2 border border-gray-200 mb-1">
+                <p class="w-[25%] text-gray-800 border-r border-gray-200 pl-2">Código</p>
+                <p class="w-[25%] text-gray-800 border-r border-gray-200 pl-2">Código</p>
+                <p class="w-[25%] text-gray-800 border-r border-gray-200 pl-2">R$</p>
+                <p class="w-[25%] text-gray-800 pl-2">Código</p>
+            </div>
         </div>
 
         <div class="flex flex-col bg-neutral-50 p-4 rounded-lg mt-5 w-1/2">
             <div class="bg-white p-4 rounded-lg w-[100%] y-1/2">
+                <div class="flex mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2 my-auto">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <h3 class="text-2xl font-bold tracking-tight text-gray-900">Lucro diário</h3>
+                </div>
             </div>
 
             <div class="bg-white p-4 rounded-lg w-[100%] y-1/2 mt-3">
