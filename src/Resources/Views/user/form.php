ec0640f14e61bc0e18878a0918f4b13922af6b70
andreeezinho <andreeesapucaia@gmail.com>
2025-09-03 20:59:26 -0300

    fix: add permission on files

--- src/Resources/Views/user/form.php
+++ src/Resources/Views/user/form.php
@@ -23,26 +23,32 @@
     <input type="text" name="telefone" id="telefone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required value="<?= $usuario->telefone ?? null?>" />
 </div>
 
-<div>
-    <label for="cargo" class="block text-sm/6 font-medium text-gray-900">Cargo</label>
-    <div class="">
-        <select name="cargo" id="cargo" value="<?= $usuario->cargo ?? null ?>" class="border-2 border-solid block w-full rounded-md bg-white px-3 p-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6">
-            <option value="" <?= (isset($usuario) && $usuario->cargo == '') ? 'selected' : null ?>>Selecione o cargo</option>
-            <option value="Administrativo" <?= (isset($usuario) && $usuario->cargo == 'Administrativo') ? 'selected' : null ?>>Administrativo</option>
-            <option value='Frente de Caixa' <?= (isset($usuario) && $usuario->cargo == 'Frente de Caixa') ? 'selected' : null ?>>Frente de Caixa</option>
-            <option value='Repositor' <?= (isset($usuario) && $usuario->cargo == 'Repositor') ? 'selected' : null ?>>Repositor</option>
-            <option value='Entregador' <?= (isset($usuario) && $usuario->cargo == 'Entregador') ? 'selected' : null ?>>Entregador</option>
-        </select>
+<?php
+    if(!$perfil){
+?>
+    <div>
+        <label for="cargo" class="block text-sm/6 font-medium text-gray-900">Cargo</label>
+        <div class="">
+            <select name="cargo" id="cargo" value="<?= $usuario->cargo ?? null ?>" class="border-2 border-solid block w-full rounded-md bg-white px-3 p-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6">
+                <option value="" <?= (isset($usuario) && $usuario->cargo == '') ? 'selected' : null ?>>Selecione o cargo</option>
+                <option value="Administrativo" <?= (isset($usuario) && $usuario->cargo == 'Administrativo') ? 'selected' : null ?>>Administrativo</option>
+                <option value='Frente de Caixa' <?= (isset($usuario) && $usuario->cargo == 'Frente de Caixa') ? 'selected' : null ?>>Frente de Caixa</option>
+                <option value='Repositor' <?= (isset($usuario) && $usuario->cargo == 'Repositor') ? 'selected' : null ?>>Repositor</option>
+                <option value='Entregador' <?= (isset($usuario) && $usuario->cargo == 'Entregador') ? 'selected' : null ?>>Entregador</option>
+            </select>
+        </div>
     </div>
-</div>
 
-<div>
-    <label for="ativo" class="block text-sm/6 font-medium text-gray-900">Situação</label>
-    <div class="">
-        <select name="ativo" id="ativo" value="<?= $usuario->ativo ?? null ?>" placeholder="Código ou nome" class="border-2 border-solid block w-full rounded-md bg-white px-3 p-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6">
-            <option value="" <?= (isset($usuario) && $usuario->ativo == '') ? 'selected' : null ?>>Insira a situação</option>
-            <option value='1' <?= (isset($usuario) && $usuario->ativo == '1') ? 'selected' : null ?>>Ativo</option>
-            <option value='0' <?= (isset($usuario) && $usuario->ativo == '0') ? 'selected' : null ?>>Inativo</option>
-        </select>
+    <div>
+        <label for="ativo" class="block text-sm/6 font-medium text-gray-900">Situação</label>
+        <div class="">
+            <select name="ativo" id="ativo" value="<?= $usuario->ativo ?? null ?>" placeholder="Código ou nome" class="border-2 border-solid block w-full rounded-md bg-white px-3 p-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6">
+                <option value="" <?= (isset($usuario) && $usuario->ativo == '') ? 'selected' : null ?>>Insira a situação</option>
+                <option value='1' <?= (isset($usuario) && $usuario->ativo == '1') ? 'selected' : null ?>>Ativo</option>
+                <option value='0' <?= (isset($usuario) && $usuario->ativo == '0') ? 'selected' : null ?>>Inativo</option>
+            </select>
+        </div>
     </div>
-</div>
+<?php
+    }
+?>
--- src/Resources/Views/user/perfil/index.php
+++ src/Resources/Views/user/perfil/index.php
@@ -2,22 +2,35 @@
     require_once __DIR__ . '/../../layout/top.php';
 ?>
 
-<div class="container pb-5">
-    <div class="row gx-3 mb-2 border-bottom pb-1">
-        <div class="col-12 col-md-6">
-            <ol class="breadcrumb mb-3">
-                <li class="breadcrumb-item">
-                    <i class="icon-house_siding lh-1"></i>
-                    <a href="/dashboard" class="text-decoration-none text-muted"><i class="bi-house"></i> Home</a>
-                </li>
-
-                <li class="breadcrumb-item">
-                    <i class="icon-house_siding lh-1"></i>
-                    <a href="/perfil" class="text-decoration-none text-muted">Seu perfil</a>
-                </li>
-            </ol>
-        </div>
-    </div>
+<div class="p-8 rounded-lg mt-14">
+    <nav class="" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li class="inline-flex items-center">
+                <a href="/dashboard" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-600">
+                    <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                    </svg>
+                    Home
+                </a>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <a href="/perfil" class="ms-1 text-sm font-medium text-gray-700 hover:text-gray-600 md:ms-2">Seu perfil</a>
+                </div>
+            </li>
+            <li aria-current="page">
+                <div class="flex items-center">
+                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">-</span>
+                </div>
+            </li>
+        </ol>
+    </nav>
 
     <?php
         if(isset($erro)){
@@ -30,24 +43,29 @@
         }
     ?>
 
-    <h3 class="my-3"><i class="bi-person-lines-fill"></i> Seu perfil</h3>
+    <h3 class="text-2xl text-center font-bold tracking-tight text-gray-900">Seu perfil</h3>
 
-    <div class="row gx-3 my-3 border-bottom pt-3">
+    <div class="my-3 pt-3">
         <form action="/perfil/icone" method="POST" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="icone" class="text-muted small d-flex flex-column text-center">
-                    <div class="d-flex position-relative mx-auto mb-2">
-                        <img src="<?= URL_SITE ?>/public/img/user/icons/<?= ($usuario->icone == "default.png" || $usuario->icone == "") ? "default.png" : $usuario->icone ?>" alt="Icone de usuario" id="preview" class="bg-secondary cadastro-icone rounded-circle mx-auto hover-border">
-                        <span class="position-absolute bottom-0 end-0"><i class="bi-camera-fill rounded-circle bg-light p-1"></i></span>
+            <div class="">
+                <label for="icone" class="text-gray-600 flex flex-col text-center">
+                    <div class="d-flex mb-3 relative mx-auto">
+                        <img src="<?= URL_SITE ?>/public/img/user/icons/<?= ($usuario->icone == "default.png" || $usuario->icone == "") ? "default.png" : $usuario->icone ?>" alt="Icone de usuario" id="preview" class="bg-gray-400 cadastro-icone rounded-[50px] mx-auto hover-border">
+                        <span class="absolute bottom-0 end-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 bg-white rounded-[25px] p-1">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
+                            </svg>
+                        </span>
                     </div>
                     Insira uma foto de perfil
                 </label>
 
-                <input type="file" name="icone" id="icone" class="d-none">
+                <input type="file" name="icone" id="icone" class="hidden">
             </div>
 
-            <div class="form-group text-center my-3">
-                <button type="submit" class="btn btn-primary">Atualizar</button>
+            <div class="text-center my-3">
+                <button type="submit" class="bg-gray-800 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">Atualizar</button>
             </div>
         </form>
     </div>
@@ -81,35 +99,6 @@
                 <button type="submit" class="btn btn-primary mx-1">Atualizar</button>
             </div>
         </form>
-    </div>
-
-    <div class="row my-3 border-bottom mt-5 pt-0 px-1">
-        <div class="col-12 alert alert-danger">
-            <h3 class="my-3 text-danger"><i class="bi-person-fill-slash"></i> Deletar conta</h3>
-            <div class="col-12 form-group text-center mt-3">
-                <button type="submit" class="btn btn-danger mx-1" data-toggle="modal" data-target="#deletar"><i class="bi-trash-fill"></i> Deletar</button>
-
-                <div class="modal fade" id="deletar" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content text-dark">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle"><i class="bi-person-fill-slash"></i> Deletar conta?</h5>
-                            </div>
-
-                            <div class="modal-body">
-                                <p class="my-auto">Deseja <b>deletar</b> sua conta?</p>
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                <form action="perfil/deletar" method="POST">
-                                    <button type="submit" class="btn btn-danger">Deletar</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </div>
     </div>
 </div>
 
