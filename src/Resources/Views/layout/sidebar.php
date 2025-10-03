8ff5aa559dd128f7324b43be31a8f7c1a3979391
andreeezinho <andreeesapucaia@gmail.com>
2025-10-03 17:35:13 -0300

    feat: add copyright span in sidebar

--- src/Resources/Views/layout/sidebar.php
+++ src/Resources/Views/layout/sidebar.php
@@ -89,12 +89,14 @@
       </ul>
 
       <div class="absolute bottom-[20px]">
-         <a href="/logout" class="flex items-center p-2 text-white rounded-lg bg-red-400 hover:bg-red-500 group">
+         <a href="/logout" class="flex items-center p-2 text-white rounded-lg bg-gray-400 hover:bg-red-400 group w-auto">
             <svg class="shrink-0 w-5 h-5 text-white transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
             </svg>
             <span class="flex-1 ms-3 whitespace-nowrap">Sair</span>
          </a>
+
+         <span class="text-gray-600 text-xs">&copy; <?= date('Y'); ?>, Desenvolvido por <a href="http://github.com/andreeezinho" class="text-gray-400 hover:underline">André Victor</a></span>
       </div>
    </div>
 </aside>
